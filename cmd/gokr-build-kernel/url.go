09fa34cacf3b8c3b8096a4752a8c3f28ad987ff5
Darshil Chanpura <darshil@thatwebsite.xyz>
2022-06-30 12:18:58 +0530

    Upgrade to version 5.18.8

--- cmd/gokr-build-kernel/url.go
+++ cmd/gokr-build-kernel/url.go
@@ -2,4 +2,4 @@
 package main
 
 // see https://www.kernel.org/releases.json
-var latest = "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.18.7.tar.xz"
+var latest = "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.18.8.tar.xz"
