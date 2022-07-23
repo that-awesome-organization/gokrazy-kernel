94cbe3781b03af87f67b3209552f2bcf2f1bd04f
Darshil Chanpura <darshil@thatwebsite.xyz>
2022-07-23 16:50:56 +0530

    Upgrade to version 5.18.14

--- cmd/gokr-build-kernel/url.go
+++ cmd/gokr-build-kernel/url.go
@@ -2,4 +2,4 @@
 package main
 
 // see https://www.kernel.org/releases.json
-var latest = "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.18.12.tar.xz"
+var latest = "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.18.14.tar.xz"
