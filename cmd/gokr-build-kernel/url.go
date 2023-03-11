881a0e1d5c83260297e5b52c8b052deaf4a44085
builder <hello@thatwebsite.xyz>
2023-03-11 16:35:02 +0530

    Upgrade to version 6.2.4

--- cmd/gokr-build-kernel/url.go
+++ cmd/gokr-build-kernel/url.go
@@ -2,4 +2,4 @@
 package main
 
 // see https://www.kernel.org/releases.json
-var latest = "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.2.3.tar.xz"
+var latest = "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.2.4.tar.xz"
