f02d8c45753bcc023029ded64911e3cea18d80e1
builder <hello@thatwebsite.xyz>
2024-02-05 16:44:14 -0500

    Upgrade to version 6.7.4

--- cmd/gokr-build-kernel/url.go
+++ cmd/gokr-build-kernel/url.go
@@ -2,4 +2,4 @@
 package main
 
 // see https://www.kernel.org/releases.json
-var latest = "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.7.3.tar.xz"
+var latest = "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.7.4.tar.xz"
